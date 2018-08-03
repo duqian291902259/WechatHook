fbb6ffe3f437210d357307ff2665edc59edaa66d
Dusan <duqian2010@gmail.com>
2018-08-03 17:15:19 +0800

    Hook wechat message,reply message automatic --duqian

--- MainApp/src/main/java/site/duqian/wchook/xposed/HookMessage.java
+++ MainApp/src/main/java/site/duqian/wchook/xposed/HookMessage.java
@@ -6,9 +6,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.text.TextUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import de.robv.android.xposed.XC_MethodHook;
 import site.duqian.wchook.base.BaseHook;
 import site.duqian.wchook.common.ApiUtil;
@@ -22,6 +19,7 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 import static de.robv.android.xposed.XposedHelpers.findClass;
 import static de.robv.android.xposed.XposedHelpers.getObjectField;
 import static de.robv.android.xposed.XposedHelpers.newInstance;
+import static de.robv.android.xposed.XposedHelpers.setObjectField;
 
 /**
  * Created by Dusan (duqian) on 2017/5/11 - 11:46.
@@ -32,76 +30,97 @@ import static de.robv.android.xposed.XposedHelpers.newInstance;
 
 public class HookMessage extends BaseHook {
 
-    private static final String TAG = HookMessage.class.getSimpleName();
+    private static final String TAG = HookMessage.class.getSimpleName() + "-wc-dq";
     private static Handler handler;
     private static Object requestCaller;
-    private static List<String> list_msg = new ArrayList<>();
+    //private static List<String> list_msg = new ArrayList<>();
 
-    private  SettingsHelper mSettings;
+    private SettingsHelper mSettings;
+
     public HookMessage(ClassLoader classLoader, Context context, SettingsHelper mSettings) {
-        super(classLoader,context);
-        if (handler==null){
-            handler=new Handler(Looper.getMainLooper());
+        super(classLoader, context);
+        if (handler == null) {
+            handler = new Handler(Looper.getMainLooper());
         }
         this.mSettings = mSettings;
     }
 
     //hook聊天列表里面的消息
     public void hookConversationItem() {
-        LogUtils.debug(TAG,"duqian  hookMessage");
-        findAndHookMethod(VersionParam.conversationClass, classLoader,
-                VersionParam.con_GetCursorMethod, Cursor.class,
-                new XC_MethodHook() {
+        LogUtils.debug(TAG, "dq hookMessage");
+        findAndHookMethod(VersionParam.conversationClass, classLoader, VersionParam.con_GetCursorMethod, Cursor.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                synchronized (HookMessage.class){
-                    String field_content  = (String) getObjectField(param.thisObject, "field_content");
-                    String field_username  = (String) getObjectField(param.thisObject, "field_username");
-                    int field_unReadCount  = (int) getObjectField(param.thisObject, "field_unReadCount");
-                    //LogUtils.debug(TAG,"hookMessage "+field_username+",content="+field_content);
-                    if (field_unReadCount == 0|| TextUtils.isEmpty(field_content)){
-                        return;
-                    }
-
-                    if (field_username.contains("微信团队")||field_username.toLowerCase().contains("wechat")
-                            ||field_username.contains("@chatroom")){
-                        return;
-                    }
-                    //I've accepted your friend request. Now let's chat! 我通过了你的朋友验证请求，现在我们可以开始聊天了
-                    if (field_content.contains("通过了你的朋友验证请求")||field_content.contains("accepted your friend request")){
-                        return;
-                    }
-
-                    if (!list_msg.contains(field_content)) {
-                        list_msg.add(field_content);
-                        boolean autoReplyMsg = autoReplyMsg(field_content);
-                        if (autoReplyMsg) {
-                            getReply(field_username,field_content);
-                        }
-                        LogUtils.debug(TAG,field_unReadCount+",autoReplyMsg="+autoReplyMsg);
-                    }
-
-                    if (list_msg.size()>60){
-                        list_msg.clear();
-                    }
+                try {
+                    hookNewMessage(param);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    LogUtils.debug(TAG, "dq hookMessage error " + e);
                 }
             }
         });
     }
 
+    private void hookNewMessage(XC_MethodHook.MethodHookParam param) {
+        synchronized (HookMessage.class) {
+            String field_content = (String) getObjectField(param.thisObject, "field_content");
+            String field_username = (String) getObjectField(param.thisObject, "field_username");
+            int field_unReadCount = (int) getObjectField(param.thisObject, "field_unReadCount");
+            int field_isSend = (int) getObjectField(param.thisObject, "field_isSend");
+            LogUtils.debug(TAG, "hookMessage " + field_username + ",field_isSend=" + field_isSend + "content=" + field_content + ",field_unReadCount=" + field_unReadCount);
+            getMsgType(param);
+            if (field_unReadCount == 0 || TextUtils.isEmpty(field_content)) {//msgType=1表示文本
+                return;
+            }
+            if (field_isSend == 1) {//自己发的不回复
+                return;
+            }
+            if (field_username.contains("微信团队") || field_username.toLowerCase().contains("wechat")
+                    || field_username.contains("@chatroom")) {
+                return;
+            }
+            //I've accepted your friend request. Now let's chat! 我通过了你的朋友验证请求，现在我们可以开始聊天了
+            if (field_content.contains("通过了你的朋友验证请求") || field_content.contains("accepted your friend request")) {
+                return;
+            }
+
+            getReply(field_username, field_content);
+        }
+    }
+
+    private void getMsgType(XC_MethodHook.MethodHookParam param) {
+        String field_msgType = (String) getObjectField(param.thisObject, "field_msgType");
+        int msgType = -1;
+        if (!TextUtils.isEmpty(field_msgType)) {
+            try {
+                msgType = Integer.parseInt(field_msgType);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        LogUtils.debug(TAG, "hookMessage msgType=" + msgType);
+
+    }
+
     //子线程请求,获取机器人回复
-    public void getReply(String field_username, String field_content) {
+    private void getReply(String field_username, String field_content) {
+        boolean autoReplyMsg = autoReplyMsg(field_content);
+        LogUtils.debug(TAG, field_content + ",autoReplyMsg=" + autoReplyMsg);
+
+        if (!autoReplyMsg) {
+            return;
+        }
         ThreadManager.getBackgroundPool().execute(new Runnable() {
             @Override
             public void run() {
                 String replyContent = ApiUtil.init().askTuringRobot(field_content);
                 if (!TextUtils.isEmpty(replyContent)) {
                     //延时回复
-                    int delayTime = WeChatHelper.getRandom(5678,12345);
-                    LogUtils.debug(TAG,"reply = "+replyContent +",delay=" +delayTime);
-                    wechatSendMessage(field_username, replyContent,delayTime);
-                }else{
-                    LogUtils.debug(TAG,"reply = null");
+                    int delayTime = WeChatHelper.getRandom(100, 1000);
+                    LogUtils.debug(TAG, "reply = " + replyContent + ",delay=" + delayTime);
+                    wechatSendMessage(field_username, replyContent, delayTime);
+                } else {
+                    LogUtils.debug(TAG, "reply = null");
                 }
             }
         });
@@ -109,29 +128,42 @@ public class HookMessage extends BaseHook {
 
     //微信发消息
     private void wechatSendMessage(String field_username, String replyContent, int delayTime) {
-        if (requestCaller==null){
+        LogUtils.debug(TAG, "------wechatSendMessage start-------");
+
+        if (requestCaller == null) {
             requestCaller = callStaticMethod(findClass(VersionParam.networkRequest, classLoader), VersionParam.requestMethod);
         }
-        Object messageRequest = newInstance(findClass(VersionParam.con_MessageClass, classLoader), field_username, replyContent,1);//type
-        callMethod(requestCaller, VersionParam.con_NetworkMethod, messageRequest, delayTime);
-        list_msg.add(replyContent);//防止回复自己发送的内容
-        LogUtils.debug(TAG,"wechatSendMessage :" + requestCaller+"，messageRequest:" + messageRequest);
+
+        Object masssendObj = newInstance(findClass("com.tencent.mm.plugin.masssend.a.a", classLoader));//
+        setObjectField(masssendObj, "laj", field_username);
+        setObjectField(masssendObj, "lak", 1);//发送给一个人，为0
+        setObjectField(masssendObj, "filename", replyContent);
+        setObjectField(masssendObj, "msgType", 1);
+        LogUtils.debug(TAG, "dq wechat masssendObj :" + masssendObj.toString());
+
+        Object messageRequestObj = newInstance(findClass("com.tencent.mm.plugin.masssend.a.f", classLoader), masssendObj, false);//
+
+        //Object messageRequestObj = newInstance(findClass(VersionParam.con_MessageClass, classLoader), field_username, replyContent, 1);//type
+
+        callMethod(requestCaller, VersionParam.con_NetworkMethod, messageRequestObj, delayTime);
+        //list_msg.add(replyContent);//缓存发送的内容
+        LogUtils.debug(TAG, "dq wechat SendMessage :" + requestCaller.toString() + "，messageRequestObj:" + messageRequestObj.toString());
     }
 
     //AI机器人回复
-    private  boolean autoReplyMsg(String content) {
+    private boolean autoReplyMsg(String content) {
         final boolean isOpenRobotReply = XspUtil.init(mSettings).isOpenRobotReply();
         if (!isOpenRobotReply) {
-            LogUtils.debug(TAG,"isOpenRobotReply="+isOpenRobotReply);
+            LogUtils.debug(TAG, "dq isOpenRobotReply=" + isOpenRobotReply);
             return false;
         }
 
         //过滤信息
-        /*final String keywords = XspUtil.init(mSettings).getFilterKeywords();
-        if (keywords.contains(content)){
-            LogUtils.debug(TAG,keywords+"   包含过滤信息，不回复: " + content);
+        final String keywords = XspUtil.init(mSettings).getFilterKeywords();
+        if (keywords.contains(content)) {
+            LogUtils.debug(TAG, keywords + ",包含过滤信息，不回复: " + content);
             return false;
-        }*/
+        }
 
         return true;
     }
--- MainApp/src/main/java/site/duqian/wchook/xposed/VersionParam.java
+++ MainApp/src/main/java/site/duqian/wchook/xposed/VersionParam.java
@@ -11,17 +11,12 @@ public class VersionParam {
     //conversation
     public static String conversationClass = PACKAGE_NAME+".e.b.ag";
     public static String con_GetCursorMethod = "b";
-    public static String con_MessageClass1 = PACKAGE_NAME+".ab.a";//message
     public static String con_NetworkMethod = "a";//a
     public static String con_MessageClass = PACKAGE_NAME+".modelmulti.i";
-    public static String activity_chatingui = PACKAGE_NAME+".ui.chatting.ChattingUI";
-    public static String activity_chatingui_a = activity_chatingui+".a";
     public static String networkRequest = PACKAGE_NAME+".model.ak";
     public static String requestMethod = "vw";
 
     //nearby friends
-    public static String NearbyFriendsUI = PACKAGE_NAME+".plugin.nearby.ui.NearbyFriendsUI";
-    public static String ContactInfoUI = PACKAGE_NAME+".plugin.profile.ui.ContactInfoUI";
     public static String SayHiEditUI = PACKAGE_NAME+".ui.contact.SayHiEditUI";
     public static String SayHiModel= PACKAGE_NAME+".pluginsdk.model.m";//打招呼
     public static String BaseRequestBean = PACKAGE_NAME+".v.k";
@@ -36,11 +31,8 @@ public class VersionParam {
                 //message
                 conversationClass = PACKAGE_NAME+".e.b.ag";
                 con_GetCursorMethod = "b";
-                con_MessageClass1 = PACKAGE_NAME+".ab.a";
                 con_NetworkMethod = "a";//a
                 con_MessageClass = PACKAGE_NAME+".modelmulti.i";
-                activity_chatingui = PACKAGE_NAME+".ui.chatting.ChattingUI";
-                activity_chatingui_a = activity_chatingui+".a";
                 networkRequest = PACKAGE_NAME+".model.ak";
                 requestMethod = "vw";
 
@@ -51,13 +43,9 @@ public class VersionParam {
                 NEARBY_PROTOCAL_POSITION2 = ".mm.plugin.nearby.a.c";
                 break;
             case "6.6.7":
-                //message
                 con_MessageClass = PACKAGE_NAME+".modelmulti.i";
-                activity_chatingui = PACKAGE_NAME+".ui.chatting.ChattingUI";
-                activity_chatingui_a = activity_chatingui+".a";
-                conversationClass = PACKAGE_NAME+".e.b.ag";
-                con_GetCursorMethod = "b";
-                con_MessageClass1 = PACKAGE_NAME+".ab.a";
+                conversationClass = PACKAGE_NAME+".g.c.am";
+                con_GetCursorMethod = "d";
 
                 networkRequest = PACKAGE_NAME+".model.au";
                 requestMethod = "DF";
